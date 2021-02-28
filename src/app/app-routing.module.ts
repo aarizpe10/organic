4460b1214026ef8ba5a4b059ee5ba5a3c258d784
Adrian Arizpe <adrian.arizpe10@gmail.com>
2021-02-27 20:47:42 -0600

    Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CarouselComponent } from './carousel/carousel.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
+  {path: 'carousel', component: CarouselComponent},
   {path: 'login', component: LoginComponent},
 
 ];
